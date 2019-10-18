6ff4b16263ef1c369c6655cb45f846fe374a3e38
Liam Johnston <liamjohnston.nz@gmail.com>
2019-10-18 13:41:16 +1300

    Updates

---
+++ precache-manifest.f36ef99736b7cb0852541326fdd57497.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6a017526f23c9ef93c7b17977d2504",
+    "url": "/comic-slack/index.html"
+  },
+  {
+    "revision": "9104babd499ef9e6dc28",
+    "url": "/comic-slack/static/css/2.fbac66a9.chunk.css"
+  },
+  {
+    "revision": "0f18fe12d84f79fe4b25",
+    "url": "/comic-slack/static/css/main.9e237501.chunk.css"
+  },
+  {
+    "revision": "9104babd499ef9e6dc28",
+    "url": "/comic-slack/static/js/2.de86bc95.chunk.js"
+  },
+  {
+    "revision": "0f18fe12d84f79fe4b25",
+    "url": "/comic-slack/static/js/main.14fa496e.chunk.js"
+  },
+  {
+    "revision": "0bf045dc65a539a78e14",
+    "url": "/comic-slack/static/js/runtime-main.f0667e87.js"
+  },
+  {
+    "revision": "574364be43983522e59831925db59885",
+    "url": "/comic-slack/static/media/add-to-slack.574364be.jpg"
+  },
+  {
+    "revision": "ee7b545935838395461f29fa6f013f38",
+    "url": "/comic-slack/static/media/comic-slack.ee7b5459.png"
+  },
+  {
+    "revision": "c9ba6021af3e9514b54e8291730c2a92",
+    "url": "/comic-slack/static/media/slack-menu.c9ba6021.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/comic-slack/precache-manifest.596cc878f9698a7208dd81f590af04d9.js"
+  "/comic-slack/precache-manifest.f36ef99736b7cb0852541326fdd57497.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/comic-slack/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/comic-slack/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

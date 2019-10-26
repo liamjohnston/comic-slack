937858bd21350ea1847d00150fcb000f5d2a06d9
Liam Johnston <liamjohnston.nz@gmail.com>
2019-10-26 20:20:32 +1300

    Overhaul. Hooks! and only convert when copying, not on every keypress.

---
+++ src/nonAlphaNumericCharMap.js
@@ -0,0 +1,14 @@
+const nonAlphaNumericCharMap = {
+  ' ': { csMapping: ':cs-s-sp:' },
+  '?': { csMapping: ':cs-s-qm:' },
+  '!': { csMapping: ':cs-s-em:' },
+  '&': { csMapping: ':cs-s-amp:' },
+  '@': { csMapping: ':cs-s-at:' },
+  ',': { csMapping: ':cs-s-com:' },
+  $: { csMapping: ':cs-s-dol:' },
+  '.': { csMapping: ':cs-s-fs:' },
+  '#': { csMapping: ':cs-s-hash:' },
+  '%': { csMapping: ':cs-s-pc:' },
+};
+
+export default nonAlphaNumericCharMap;
